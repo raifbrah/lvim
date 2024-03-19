785b12d2248977b063618b3eae96392798abb247
raifbrah <raifbrah@gmail.com>
2024-03-19 20:32:58 +0300

    Init repo

---
+++ lua/user/plugins.lua
@@ -0,0 +1,18 @@
+lvim.plugins = {
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      -- "nvim-tree/nvim-tree.lua", -- В Lunarvim уже есть nvim-tree и если тут его не выключить, начинаются конфликты.
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
+  {
+    "karb94/neoscroll.nvim",
+    config = function ()
+      require('neoscroll').setup {}
+    end
+  },
+}
