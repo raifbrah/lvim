735661643c158012b532694572748999181b06bc
raifbrah <raifbrah@gmail.com>
2024-03-22 01:17:39 +0300

    Added new theme Catppuccin (catppuccin-latte)

--- config.lua
+++ config.lua
@@ -1,3 +1,5 @@
 require("user.options")
 require("user.plugins")
 require("user.keybindings")
+
+lvim.colorscheme = "catppuccin-latte"
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -15,4 +15,9 @@ lvim.plugins = {
       require('neoscroll').setup {}
     end
   },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000
+  }
 }
