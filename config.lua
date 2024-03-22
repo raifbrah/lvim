5d8e3731571885f3eaf65c95defac442ff9660ec
raifbrah <raifrah@gmail.com>
2024-03-23 00:04:20 +0300

    Added code formater (Prettier)

--- config.lua
+++ config.lua
@@ -3,3 +3,9 @@ require("user.plugins")
 require("user.keybindings")
 
 lvim.colorscheme = "catppuccin"
+
+-- Code formatters setup
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { name = "prettier" }
+}
