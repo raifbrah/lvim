6e41f2334a4e9d3cb8eb5bb090aeb243f49efe91
raifbrah <raifbrah@gmail.com>
2024-03-25 20:53:30 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -15,3 +15,18 @@ local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   { name = "prettier" }
 }
+
+lvim.builtin.nvimtree.setup.view.width = 45
+
+lvim.builtin.telescope.defaults = {
+  layout_strategy = "vertical",
+  layout_config = {
+    vertical = {
+      prompt_position = "top",
+      width = { padding = 0 },
+      height = { padding = 0 },
+      preview_height = 0.4,
+    },
+  },
+  sorting_strategy = "ascending",
+}
